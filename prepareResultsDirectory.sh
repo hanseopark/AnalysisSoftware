c507f2c589a89f2dd0e537c2dc67dde72fe283e2
Friederike Bock <friederike.bock@cern.ch>
2016-04-26 17:36:32 +0200

    adapted prepare script

--- prepareResultsDirectory.sh
+++ prepareResultsDirectory.sh
@@ -18,6 +18,7 @@ mkdir ExternalInputpPb
 mkdir FinalResults
 mkdir CocktailInput
 mkdir RooUnfold
+mkdir DownloadAndDataPrep
 ln -sf $PHOTONCONVDIR/SystematicErrorsNew/* SystematicErrorsNew/
 ln -sf $PHOTONCONVDIR/CommonHeaders/*.h CommonHeaders/
 ln -sf $PHOTONCONVDIR/TaskV1/*.h TaskV1/
@@ -30,16 +31,12 @@ ln -sf $PHOTONCONVDIR/ExternalInputpPb/* ExternalInputpPb/
 ln -sf $PHOTONCONVDIR/FinalResults/* FinalResults/
 ln -sf $PHOTONCONVDIR/CocktailInput/* CocktailInput/
 ln -sf $PHOTONCONVDIR/RooUnfold/* RooUnfold/
+ln -sf $PHOTONCONVDIR/DownloadAndDataPrep/* DownloadAndDataPrep/
 
 ln -sf $PHOTONCONVDIR/*.eps .
-ln -sf $PHOTONCONVDIR/*.root .
 ln -sf $PHOTONCONVDIR/*.C .
-ln -sf $PHOTONCONVDIR/*.dat .
 ln -sf $PHOTONCONVDIR/*.h .
 ln -sf $PHOTONCONVDIR/*.sh .
-ln -sf $PHOTONCONVDIR/*.pl .
-ln -sf $PHOTONCONVDIR/binNumbers*.txt .
-ln -sf $PHOTONCONVDIR/runNumbers*.txt .
 
 if [ $2 = "pp2760GeV" ]; then
     rm *PbPb*
@@ -49,7 +46,4 @@ if [ $2 = "pp2760GeV" ]; then
     rm *13TeV*
     rm *LHC11h*
     rm *LHC10*
-    rm *LHC13b*
-    rm *LHC13e7*
-    rm *LHC14*
 fi
