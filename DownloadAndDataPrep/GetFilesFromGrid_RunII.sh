e9103255a8f4fda09cc7fedbef65323733a8bf7d
mdanisch <meike.charlotte.danisch@cern.ch>
2016-07-28 15:36:11 +0200

    combined download scripts

--- DownloadAndDataPrep/GetFilesFromGrid_RunII.sh
+++ DownloadAndDataPrep/GetFilesFromGrid_RunII.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 
-# This macro copies GammaConvV1_<trainconfig>.root files from grid and puts them in the correct directories
+# This macro copies GammaConvV1_<trainConfig> or AnalysisResults.root files from grid and puts them in the correct directories
 
 #****** arguments ******************************************************************************************************************
 # $1 = period 
-# $2 = "runwise" (download runwise and optional merge afterwards) / 
-#      "merge"   (merge ONLY)
+# $2 = "G"       (Download GammaConv files)
+#      "A"       (Download AnalysisResults.root files)
+# $3 = "runwise" (download runwise. If $2="G": optional merge afterwards) 
+#      "merge"   (merge ONLY. Only for $2="G")
 
 #***** variables to be set manually ******************************************************************************************************************
-# specify name of runlist on grid:
-RUNLIST=merge
-
 # specify where the files should be saved:
-# will be $BASEDIR/$SYSTEM/$PERIOD/$RUNLIST/GammaConvV1_<trainconfig>.root
-#         $BASEDIR/$SYSTEM/$PERIOD/$run/GammaConvV1_<trainconfig>.root
-BASEDIR=/home/meike/Data/GridOutput/GridOutputGammaConv
-
+#     $DIR/GammaConv/$SYSTEM/$PERIOD/$run/GammaConvV1_<trainconfig>.root
+# or  $DIR/PhotonQA/$SYSTEM/$PERIOD/$run/AnalysisResults.root
+DIR=/home/meike/analysis/data/GridOutput
 
 #****** automatic settings according to period given as argument ****************
 # update when there is a new production or a new production pass
@@ -28,11 +26,9 @@ case "$1" in    # default: echo "no valid period chosen";
 	isMC=FALSE
 	SYSTEM=pp
 	PASS=pass2
-	# 56 runs AOD produced
-	#RUN=(225000 225011 225016 225026 225031 225035 225037 225041 225043 225050 225051 225052 225106 225305 225307 225309 225310 225313 225314 225315 225322 225576 225578 225579 225580 225582 225586 225587 225705 225707 225708 225709 225710 225716 225717 225719 225753 225757 225762 225763 225766 225768 226062 226170 226220 226225 226444 226445 226452 226466 226468 226472 226476 226483 226495 226500);
-	# 53 runs good
-	RUN=(225026 225031 225035 225037 225041 225043 225050 225051 225052 225106 225305 225307 225309 225310 225313 225314 225315 225322 225576 225578 225579 225580 225582 225586 225587 225705 225707 225708 225709 225710 225716 225717 225719 225753 225757 225762 225763 225766 225768 226062 226170 226220 226225 226444 226445 226452 226466 226468 226472 226476 226483 226495 226500);
-	AODNO=171 # was: REFILTERING
+	# listPCMgood_kINT7:
+	RUN=(225026 225031 225035 225037 225041 225043 225050 225051 225052 225106 225305 225307 225309 225310 225313 225314 225315 225322 225576 225578 225579 225586 225587 225705 225707 225708 225709 225710 225716 225717 225719 225753 225757 225762 225763 225766 225768 226062 226170 226220 226225 226444 226445 226452 226466 226468 226472 226476 226483 226495 226500);
+	AODNO=171
 	;;
     "LHC15g3a3") # anchored to 15f	
 	PERIOD=LHC15g3a3
@@ -41,9 +37,7 @@ case "$1" in    # default: echo "no valid period chosen";
 	SYSTEM=pp
 	# no pass
 	AODNO=176
-	# 54 runs produced:
-	#RUN=(225000 225011 225016 225026 225031 225035 225037 225041 225043 225050 225051 225052 225106 225305 225307 225309 225310 225313 225314 225315 225322 225576 225578 225579 225586 225587 225705 225707 225708 225709 225710 225716 225717 225719 225753 225757 225762 225763 225766 225768 226062 226170 226220 226225 226444 226445 226452 226466 226468 226472 226476 226483 226495 226500);
-	# 51 runs good: 
+	# listPCMgood_kINT7: 
 	RUN=(225026 225031 225035 225037 225041 225043 225050 225051 225052 225106 225305 225307 225309 225310 225313 225314 225315 225322 225576 225578 225579 225586 225587 225705 225707 225708 225709 225710 225716 225717 225719 225753 225757 225762 225763 225766 225768 226062 226170 226220 226225 226444 226445 226452 226466 226468 226472 226476 226483 226495 226500);
 	;;
     "LHC15g3c3") # anchored to 15f
@@ -53,7 +47,7 @@ case "$1" in    # default: echo "no valid period chosen";
 	SYSTEM=pp
 	# no pass
 	AODNO=176
-	# 54 runs produced:
+	# listPCMgood_kINT7:
 	RUN=(225026 225031 225035 225037 225041 225043 225050 225051 225052 225106 225305 225307 225309 225310 225313 225314 225315 225322 225576 225578 225579 225586 225587 225705 225707 225708 225709 225710 225716 225717 225719 225753 225757 225762 225763 225766 225768 226062 226170 226220 226225 226444 226445 226452 226466 226468 226472 226476 226483 226495 226500);
 	;;
     "LHC15g")
@@ -79,23 +73,43 @@ case "$1" in    # default: echo "no valid period chosen";
 	PASS=pass1
 	RUN=(236866 236863 236862 236860 236850 236848 236835 236824 236822 236821 236816 236815 236814 236813 236569 236565 236564 236563 236562 236558 236556 236444 236443 236441  236393 236389 236360 236359 236357 236356 236354 236353 236352 236349 236348 236337 236334 236331 236284 236281 236248 236246 236244 236242 236240 236238 236234 236227 236222 236204 236203 236164 236163 236161 236158 236153 236151 236150 236138 236137 236062 235898 235897 235896 235895 235893 235891 235890 235889 235888 235886 235841 235839 235811 235759 235721 235714 235710 235694 235684 235683 235573 235547 235462 235459 235454 235450 235443 235436 235435 235432 235423 235383 235380 235364 235362 235356 235347 235345 235344 235245 235242 235226 235204 235203 235201 235196);
 	;;
-    "LHC15o")
+    "LHC15oLowIR")
 	PERIOD=LHC15o
 	YEAR=2015
 	isMC=FALSE
 	SYSTEM=PbPb
-	PASS=pass_lowint_firstphys
+	PASS=pass2_lowIR
+	# all 13 runs:
 	RUN=(244917 244918 244975 244980 244982 244983 245061 245064 245066 245068 246390 246391 246392);
 	;;
-
+    "LHC15oHighIR")
+	PERIOD=LHC15o
+	YEAR=2015
+	isMC=FALSE
+	SYSTEM=PbPb
+	PASS=pass1
+	# all 31 runs:
+	RUN=(245683 246807 246808 246809 246810 246844 246845 246846 246847 246851 246855 246858 246859 246864 246865 246867 246870 246871 246928 246930 246937 246942 246945 246948 246949 246980 246982 246984 246989 246991 246994);
+	;;
 esac
 
 echo "chose period: " $PERIOD", year: "$YEAR", MC: "$isMC", pass: "$PASS", collision system: "$SYSTEM", refiltering number if available: "$AODNO;
 
+if [ $2 = "G" ]; then
+    echo "Chose GammaConvV1_<trainconfig>.root";
+    FILETYPE=G
+    BASEDIR=$DIR/GammaConv/
+elif [ $2 = "A" ]; then
+    echo "Chose AnalysisResults.root"
+    FILETYPE=A
+    BASEDIR=$DIR/PhotonQA/
+else
+    echo "second argument should be G or A";
+fi
 
 # ********* common functions **********************************************************************************************************
 Structure() {
-
+    if [ "$FILETYPE" == "G" ]; then
 	ls $OUTPUTDIR/GammaConvV1_*.root > fileData.txt
 	fileNumbers=`cat fileData.txt`
 	for fileName in $fileNumbers; do
@@ -103,6 +117,7 @@ Structure() {
 	    echo "trainconfig: " $number
 	    root -l -b -q -x ChangeStructureToStandard.C\(\"$OUTPUTDIR/GammaConvV1_$number.root\"\,\"$OUTPUTDIR/GammaConvV1.root\"\,\"GammaConvV1_$number\"\)
 	done
+    fi
 }
 
 Merge() {
@@ -147,7 +162,7 @@ WriteInfo() {
 	    answerInfo=no;
 	fi
 	if [ "$answerInfo" == "yes" ]; then
-	   echo -e "$TRAIN\n$PERIOD\n$PASS" > $BASEDIR/$SYSTEM/$PERIOD/info.txt  # write also ESD/AOD
+	   echo -e "$TRAIN\n$PERIOD\n$PASS" > $BASEDIR/$SYSTEM/$PERIOD/info.txt  # write also ESD/AOD, AliPhysics version!!
 	   echo "info.txt written"
 	   cat $BASEDIR/$SYSTEM/$PERIOD/info.txt
 	fi
@@ -200,16 +215,20 @@ else # MC
 fi
 
 for run in "${RUN[@]}"; do
-    #echo $run;
     if [ "$isMC" == "FALSE" ];then # data
 	SOURCEDIR=/alice/data/$YEAR/$PERIOD/000$run/$PASS$AOD/PWGGA/$TRAINDIR
     else
 	SOURCEDIR=/alice/sim/$YEAR/$PERIOD/$run$AOD/PWGGA/$TRAINDIR
     fi
-    expr=$SOURCEDIR/[[:digit:]]*_[[:digit:]]*-[[:digit:]]*/GammaConvV1_[[:digit:]]*.root
-    echo "search "$SOURCEDIR;
-    alien_find $SOURCEDIR/*/ GammaConvV1_*.root | grep $expr
-        
+    if [ "$FILETYPE" == "G" ]; then
+	expr=$SOURCEDIR/[[:digit:]]*_[[:digit:]]*-[[:digit:]]*/GammaConvV1_[[:digit:]]*.root
+	echo "search "$SOURCEDIR;
+	alien_find $SOURCEDIR/*/ GammaConvV1_*.root | grep $expr
+    else
+	expr=$SOURCEDIR/[[:digit:]]*_[[:digit:]]*-[[:digit:]]*/AnalysisResults.root
+	echo "search "$SOURCEDIR;
+	alien_find $SOURCEDIR/*/ AnalysisResults.root | grep $expr
+    fi        
 done
 
 }
@@ -217,27 +236,27 @@ done
 #******* MAIN ROUTINE ***********************************************************************************************************************
 
 # ********************* automatic settings to according to second argument ***************************
-if [ $2 = "runwise" ]; then
+if [ $3 = "runwise" ]; then
     echo "chose runwise download";
-elif [ $2 = "merge" ]; then
+elif [ $3 = "merge" ]; then
     Merge
     exit
 else
-    echo "second argument should be runwise, merged or merge";
+    echo "third argument should be runwise or merge";
 fi
 
-    #******* SELECT TRAIN **********          if not $2 = merged!
+    #******* SELECT TRAIN **********          if not $3 = merged!
     echo "available data from ESDs: "
     isAOD=FALSE
-    Show
+    Show 
     echo "available data from AODs: "
     isAOD=TRUE
-    Show
+    Show 
     echo "which train do you want to use?";
     read TRAIN
     echo "chose train: "$TRAIN;
 
-    # defined: BASEDIR RUNLIST
+    # defined: BASEDIR 
     #          PERIOD YEAR isMC SYSTEM PASS RUN AODNO
     #          TRAIN
     # not usable: TRAINDIR SOURCEDIR AOD expr isAOD
@@ -285,12 +304,12 @@ fi
 	echo "isMC mus be TRUE or FALSE. Is "$isMC;
     fi
 
-    # defined: BASEDIR RUNLIST
+    # defined: BASEDIR 
     #          PERIOD YEAR isMC SYSTEM PASS RUN AODNO
     #          TRAIN isAOD TRAINDIR PREFIX AOD
     # not usable: SOURCEDIR expr 
 
-    #check if there is already data for this period        if not $2 = merged!!
+    #check if there is already data for this period        if not $3 = merged!!
     OldData
 
     #***** RUNWISE ************* download runwise and ask afterwards for merging
@@ -302,7 +321,11 @@ fi
 	    echo "Run $i of ${#RUN[@]} ...";
 	    SOURCEDIR=/alice/$TYPE/$YEAR/$PERIOD/$PREFIX$run/$PASS/$AOD/PWGGA/$TRAINDIR/$TRAIN 
 	    OUTPUTDIR=$BASEDIR/$SYSTEM/$PERIOD/$run/
-	    alien_ls $SOURCEDIR/GammaConvV1_*.root > filesToCopy.txt
+	    if [ $2 = "G" ]; then
+		alien_ls $SOURCEDIR/GammaConvV1_*.root > filesToCopy.txt
+	    else
+		alien_ls $SOURCEDIR/AnalysisResults.root > filesToCopy.txt
+	    fi
 	    files=`cat filesToCopy.txt`
 	    if [ "$files" = "" ]; then
 		continue
@@ -310,7 +333,7 @@ fi
 	    mkdir -p $OUTPUTDIR  # move below ?
 	    EXISTS=`ls $OUTPUTDIR`
 	    if [ "$EXISTS" != "" ] && [ "$answer" != "all" ] && [ "$answer" != "none" ]; then
-	        echo "files already exist for this run: "$EXISTS" download anyways? yes / all / no / none";
+	        echo "files already exist for this run: "$EXISTS" download anyways? yes / all / no / none";  # !!! Diese Abfrage funktioniert noch nicht
 		read answer
 		if [ "$answer" != "yes" ] && [ "$answer" != "no" ] && [ "$answer" != "all" ] && [ "$answer" != "none" ]; then
 		    echo "no valid answer has been given";
@@ -328,20 +351,21 @@ fi
 		    #Change structure to standard
 		    Structure
 		fi
-	    done                                                          
+	    done  
 	done
 	WriteInfo
-	echo "Do you want to merge the given run list? yes / no";
-	read answerMerge
-	if [ "$answerMerge" != "yes" ] && [ "$answerMerge" != "no" ]; then
-	    echo "no valid answer has been given. Will assume no.";
-	    answerMerge=no;
-	fi
-	if [ "$answerMerge" == "yes" ]; then 
-	    Merge
+	if [ $2 = "G" ]; then
+	    echo "Do you want to merge the given run list? yes / no";
+	    read answerMerge
+	    if [ "$answerMerge" != "yes" ] && [ "$answerMerge" != "no" ]; then
+		echo "no valid answer has been given. Will assume no.";
+		answerMerge=no;
+	    fi
+	    if [ "$answerMerge" == "yes" ]; then 
+		Merge
+	    fi
 	fi
 	
-
 # remove help files
 if [ -f fileData.txt ]; then rm fileData.txt; fi
 if [ -f filesToCopy.txt ]; then rm filesToCopy.txt; fi
