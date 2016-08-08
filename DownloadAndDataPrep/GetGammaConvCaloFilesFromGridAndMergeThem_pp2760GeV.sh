7fd76839b2c718690916ac5c634f5b5f6610d57d
fbock <friederike.bock@cern.ch>
2016-08-08 13:22:17 +0200

    updated download scripts with latest configuration for 2.76TeV

--- DownloadAndDataPrep/GetGammaConvCaloFilesFromGridAndMergeThem_pp2760GeV.sh
+++ DownloadAndDataPrep/GetGammaConvCaloFilesFromGridAndMergeThem_pp2760GeV.sh
@@ -98,257 +98,6 @@ elif [ $1 = "dmuhlhei" ]; then
 fi
 
 
-# TRAINDIR=Legotrain-ConvCalo-LHC11a-Sys-MB #
-# LHC11aData="946"; #1,3,4,5
-# LHC11aData="952"; #6,7,8,9 
-# LHC11aData="948"; #10,11,12,14
-# LHC12f1aMC="988"; #1,3,4,5
-# LHC12f1aMC="989"; #6,7,8,9
-# LHC12f1aMC="990"; #10,11,12,14
-# LHC12f1bMC="1046"; #1,3,4,5
-# LHC12f1bMC="992"; #6,7,8,9
-# LHC12f1bMC="993"; #10,11,12,14
-# LHC12i3MC="975"; #1,3,4,5
-# LHC12i3MC="976"; #6,7,8,9
-# LHC12i3MC="977"; #10,11,12,14
-# LHC15g1aMC="1168";  #1,3,4 - merging jobs failed - fixed
-# LHC15g1aMC="1169";  #5,6,7 - merging jobs failed - fixed
-# LHC15g1aMC="1170";  #8,9,10 - fixed
-# LHC15g1aMC="1171";  #11,12,14 -fixed
-
-# TRAINDIR=Legotrain-ConvCalo-LHC11a-Sys-EMC1
-# LHC11aData="951"; #16,17,18,19
-# LHC11aData="1020"; #20,21,22,23 
-# LHC11aData="953"; #24,25,26
-# LHC11aData="955"; #27
-# LHC12f1aMC="1043"; #16,17,18,19
-# LHC12f1aMC="995"; #20,21,22,23
-# LHC12f1aMC="1148"; #24,25,26,27 
-# LHC12f1bMC="1045"; #16,17,18,19
-# LHC12f1bMC="1149"; #20,21,22,23 
-# LHC12f1bMC="999"; #24,25,26,27
-# LHC12i3MC=""; #
-# LHC15g1aMC="1191";  #16,17,18 - merging jobs failed - fixed
-# LHC15g1aMC="1192";  #19,20,21 - merging jobs failed - fixed
-# LHC15g1aMC="1193";  #22,23,24 - merging jobs failed
-# LHC15g1aMC="1194";  #25,26,27
-
-# TRAINDIR=Legotrain-ConvCalo-LHC11a-Sys-lowE
-# LHC11aData="1021"; #2,15
-# LHC12f1aMC="1000"; #2,15
-# LHC12f1bMC="1001"; #2,15
-# LHC12i3MC="1002"; # 2,15
-# LHC15g1aMC="1181";  #2, 15
-
-# TRAINDIR=Legotrain-ConvCalo-LHC11a-Sys-V1Cluster
-# LHC11aData="1009"; #2,15
-# LHC12f1aMC="1182"; #2,15
-# LHC12f1bMC="1183"; #2,15
-# LHC12i3MC="1184"; # 2,15
-# LHC15g1aMC="1180";  #2, 15
-
-# TRAINDIR=Legotrain-ConvCalo-LHC13g-Sys-INT7
-# LHC13gData="956_20150906-1145"; #41,43,44,45
-# LHC13gData="957_20150906-1146"; #46,47,48,49
-# LHC13gData="958_20150906-1147"; #50,51,52,54
-# LHC15a3aMC="1172_20151009-1721";  #43,44,45 
-# LHC15a3aMC="1173_20151009-1721";  #46,47,48 - merging jobs failed - fixed
-# LHC15a3aMC="1174_20151009-1729";  #49,50,51
-# LHC15a3aMC="1175_20151009-1727";  #52,54
-# LHC15a3aMC="1240_20151015-2232";  #41
-# LHC15a3aplusMC="1176_20151009-1732";  #43,44,45 - merging jobs failed - fixed
-# LHC15a3aplusMC="1177_20151009-1725";  #46,47,48 - merging jobs failed - fixed
-# LHC15a3aplusMC="1178_20151009-1733";  #49,50,51
-# LHC15a3aplusMC="1179_20151009-1731";  #52,54
-# LHC15a3aplusMC="1241_20151015-2233";  #41
-# LHC15g2MC="1009_20150909-1232"; #43,44,45,46
-# LHC15g2MC="1152_20151006-1511"; #47,48,49,50 
-# LHC15g2MC="1011_20150909-1416"; #51,52,54
-# LHC15g2MC="1242_20151015-2234"; #41
-
-
-# TRAINDIR=Legotrain-ConvCalo-LHC13g-Sys-EMC7
-# LHC13gData="960"; # 56,57,58,59 
-# LHC13gData="961"; # 60,61,62,64 
-# LHC13gData="962"; # 65,66,68
-# LHC13gData="1024"; #63
-# LHC15a3aMC="1199"; # 56,57,58 
-# LHC15a3aMC="1200"; # 59,60,61 
-# LHC15a3aMC="1201"; # 62,63,64 
-# LHC15a3aMC="1202"; # 65,68 
-# LHC15a3aplusMC="1195";  # 56,57,58 
-# LHC15a3aplusMC="1196";  # 59,60,61
-# LHC15a3aplusMC="1197";  # 62,63,64 
-# LHC15a3aplusMC="1198";  # 65,68 
-
-# TRAINDIR=Legotrain-ConvCalo-LHC13g-Sys-EG2
-# LHC13gData="963"; # 70,71,72,73
-# LHC13gData="964"; # 74,75,76,77
-# LHC13gData="965"; # 78,79,81
-# LHC15a3aMC="1207"; # 70,71,72
-# LHC15a3aMC="1208"; # 73,74,75 
-# LHC15a3aMC="1209"; # 76,77,78 
-# LHC15a3aMC="1210"; # 79, 81 
-# LHC15a3aplusMC="1203";  # 70,71,72
-# LHC15a3aplusMC="1204";  # 73,74,75
-# LHC15a3aplusMC="1205";  # 76,77,78
-# LHC15a3aplusMC="1206";  # 79,81
-
-# TRAINDIR=Legotrain-ConvCalo-LHC13g-Sys-EG1
-# LHC13gData="966"; # 83,84,85,86 
-# LHC13gData="967"; # 87,88,89,90
-# LHC13gData="968"; # 91,92,94
-# LHC15a3aMC="1211"; # 83,84,85
-# LHC15a3aMC="1212"; # 86,87,88 
-# LHC15a3aMC="1213"; # 89,90,91
-# LHC15a3aMC="1214"; # 92,94
-# LHC15a3aplusMC="1215"; # 83,84,85
-# LHC15a3aplusMC="1216"; # 86,87,88 
-# LHC15a3aplusMC="1217"; # 89,90,91
-# LHC15a3aplusMC="1218"; # 92,94
-
-# TRAINDIR=Legotrain-ConvCalo-LHC13g-Sys-lowE - downloaded
-# LHC13gData="959"; # 42,55,69,82
-# LHC15a3aMC="1186";  # 42,55
-# LHC15a3aplusMC="1185";  # 42,55,69,82
-# LHC15g2MC="1187"; # 42,55,69,82
-
-# TRAINDIR=Legotrain-ConvCalo-LHC13g-Sys-V1Cluster - downloaded
-# LHC13gData="1022"; # 41
-# LHC15a3aMC="1189";  # 41
-# LHC15a3aplusMC="1190";  # 41
-# LHC15g2MC="1188"; # 41
-
-# TRAINDIR=Legotrain-vAN-20151019-2.76TeV-QA
-#LHC13gData="1034"; # 95,96
-#LHC15a3aMC="1249";  # 95,96
-#LHC15a3aplusMC="1250";  # 95,96
-#LHC15g2MC="1248"; # 95,96
-# LHC11aData="1035"; #1
-# LHC12f1aMC="1245"; #1
-# LHC12f1bMC="1246"; #1
-# LHC15g1aMC="1247";  #1
-
-# TRAINDIR=Legotrain-vAN-20151025-MultiplicityDependence
-# # LHC11aData="371";
-# LHC12f1aMC="1258"; #calo98, convCalo98
-# LHC12f1bMC="1259"; #calo98, convCalo98
-# LHC15g1aMC="1262"; #calo98, convCalo98
-# 
-# # LHC15a3aMC="1260"; 
-# # LHC15a3aplusMC="1261"; 
-# # LHC15g2MC="1263"; 
-# LHC15g2MC="1264"; 
-
-# TRAINDIR=Legotrain-vAN-20151103-MultiplicityDependence2
-# # LHC11aData="371";
-# LHC12f1aMC="1273"; #calo98, convCalo98
-# LHC12f1bMC="1274"; #calo98, convCalo98
-# LHC15g1aMC="1275"; #calo98, convCalo98
-
-# LHC15a3aMC="1260"; 
-# LHC15a3aplusMC="1261"; 
-# LHC15g2MC="1263"; 
-# LHC15g2MC="1264"; 
-
-# TRAINDIR=Legotrain-vAN-20151106-MultiplicityDependence3
-# # LHC11aData="371";
-# LHC12f1aMC="1278"; 
-# LHC12f1bMC="1279"; 
-# # LHC15g1aMC="1275"; 
-
-# TRAINDIR=Legotrain-vAN-20151101-NonLinearityStudies
-# LHC11aData="1059";
-# LHC12f1aMC="1270"; 
-# LHC12f1bMC="1271"; 
-# LHC15g1aMC="1275"; 
-
-# TRAINDIR=Legotrain-vAN-20151211-MultiplicityDependence4
-# LHC12f1aMC="1364"; 
-# LHC12f1bMC="1365"; 
-# LHC15g2MC="1363"; 
-
-# TRAINDIR=Legotrain-vAN-20151213-CalibDefaultTiming
-# LHC11aData="1125";
-# LHC12f1aMC="1360"; 
-# LHC12f1bMC="1361"; 
-# LHC15g1aMC="1388";
-# LHC12i3MC="1385";
-  
-# LHC13gData="1126"
-# LHC15g2MC="1362";
-
-# TRAINDIR=Legotrain-vAN-20151213-CalibOpenTiming
-# LHC11aData="1127";
-# LHC12f1aMC="1360"; 
-# LHC12f1bMC="1361"; 
-
-# LHC13gData="1128"
-# LHC15g2MC="1362";
-
-# TRAINDIR=Legotrain-vAN-20151213-CalibOpenTimingDeltaT
-# LHC11aData="1129";
-# LHC12f1aMC="1360"; 
-# LHC12f1bMC="1361"; 
-# 
-# LHC13gData="1130"
-# LHC15g2MC="1362";
-
-# TRAINDIR=Legotrain-vAN-20151218-TriggersWithoutPileupAndPCMTriggers
-# LHC13gData="1136"
-# LHC15g2MC="1373";
-# LHC15a3aMC="1377"; 
-# LHC15a3aplusMC="1375"; 
-
-# LHC13gData="1137"
-# LHC15g2MC="1374";
-# LHC15a3aMC="1378"; 
-# LHC15a3aplusMC="1376"; 
-# LHC11aData="1135";
-# LHC12f1aMC="1371"; 
-# LHC12f1bMC="1372"; 
-# LHC15g1aMC="1379"; 
-
-# TRAINDIR=Legotrain-vAN-20160108-CalibOpenTiming
-# LHC11aData="1169";
-# LHC12f1aMC="1467"; 
-# LHC12f1bMC="1468"; 
-# LHC15g1aMC="1470";
-# LHC12i3MC="1469";
-
-# LHC13gData="1170"
-# LHC15g2MC="1475";
-# LHC15a3aMC="1472"; 
-# LHC15a3aplusMC="1473"; 
-
-# TRAINDIR=Legotrain-vAN-20160108-CalibOpenTimingDeltaT
-# LHC11aData="1171";
-# # LHC12f1aMC="1467"; 
-# # LHC12f1bMC="1468"; 
-# # LHC15g1aMC="1470";
-# # LHC12i3MC="1469";
-# 
-# LHC13gData="1172"
-# # LHC15g2MC="1475";
-# # LHC15a3aMC="1472"; 
-# # LHC15a3aplusMC="1473"; 
-
-# TRAINDIR=Legotrain-vAN-20160124-NLCorrV1
-# LHC11aData="1176";
-# LHC12f1aMC="1526"; 
-# LHC12f1bMC="1527"; 
-# LHC12i3MC="1528";
-# LHC15g1aMC="1529";
-# 
-# # LHC13gData="1179"
-# # LHC15g2MC="1530";
-# # LHC15g2MC="1531";
-# # LHC15a3aMC="1532"; 
-# # LHC15a3aMC="1535"; 
-# LHC15a3aMC="LHC15a3a"; 
-# # LHC15a3aplusMC="1533"; 
-# # LHC15a3aplusMC="1534"; 
-# LHC15a3aplusMC="LHC15a3aplus"; 
 
 # TRAINDIR=Legotrain-SysAdd-ConvCalo
 # # LHC11aData="1209";
@@ -431,16 +180,28 @@ fi
 # LHC15a3aplusMC="2124"; 
 # 
 
-TRAINDIR=Legotrain-Rerun-FullCalo-vAN20160601_wSort
-LHC11aData="1564";
-LHC12f1aMC="2152"; 
-LHC12f1bMC="2153"; 
-LHC15g1aMC="2155";
+# TRAINDIR=Legotrain-Rerun-FullCalo-vAN20160601_wSort
+# LHC11aData="1564";
+# LHC12f1aMC="2152"; 
+# LHC12f1bMC="2153"; 
+# LHC15g1aMC="2155";
+# 
+# LHC13gData="1568"
+# LHC15g2MC="2154";
+# LHC15a3aMC="2150"; 
+# LHC15a3aplusMC="2151"; 
 
-LHC13gData="1568"
-LHC15g2MC="2154";
-LHC15a3aMC="2150"; 
-LHC15a3aplusMC="2151"; 
+TRAINDIR=Legotrain-mCalo-20160727_SecEffiAndTMStudiesRerun
+LHC11aData="1564"; 
+LHC15g1aMC="2353";
+LHC12f1aMC="2350"; 
+LHC12f1bMC="2351"; 
+
+LHC13gData="1568"; 
+LHC15a3aMC="2348"; 
+LHC15a3aplusMC="2349"; 
+LHC15g2MC="2352";
+
 
 OUTPUTDIR=$BASEDIR/$TRAINDIR
 
