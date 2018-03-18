139a3ea0d8fc98edb7f441415897d1d2a226d995
Nicolas Schmidt <nicolas.schmidt@cern.ch>
2018-03-18 14:28:53 +0100

    added inv. yield in addition to HEPdata for dir gamma pp paper

--- ProduceHEPDataFileForPPDirGammaPaper2018.C
+++ ProduceHEPDataFileForPPDirGammaPaper2018.C
@@ -78,24 +78,34 @@ void ProduceHEPDataFileForPPDirGammaPaper2018(
 	
 	TFile* fCombResultspp2760GeV= new TFile(nameFilepp2760GeV.Data());
 		TDirectory* 		directoryCombGammapp2760GeV 					  = (TDirectory*)fCombResultspp2760GeV->Get("Gamma2.76TeV");
-		TGraphAsymmErrors*	graphDirGammaYieldStatpp2760GeV 		= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionDirGammaNonFitStatErr");
-		TGraphAsymmErrors*	graphDirGammaYieldSyspp2760GeV 			= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionDirGammaNonFitSysErr");
-    TGraphAsymmErrors*	graphDirGammaYieldUpperpp2760GeV 		= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionDirGammaNonFitSumErrAr");
+		TGraphAsymmErrors*	graphDirGammaYieldStatpp2760GeV 		= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvYieldDirGammaNonFitStatErr");
+		TGraphAsymmErrors*	graphDirGammaYieldSyspp2760GeV 			= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvYieldDirGammaNonFitSysErr");
+    TGraphAsymmErrors*	graphDirGammaYieldUpperpp2760GeV 		= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvYieldDirGammaNonFitSumErrAr");
+		TGraphAsymmErrors*	graphDirGammaXSectionStatpp2760GeV 	= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionDirGammaNonFitStatErr");
+		TGraphAsymmErrors*	graphDirGammaXSectionSyspp2760GeV 	= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionDirGammaNonFitSysErr");
+    TGraphAsymmErrors*	graphDirGammaXSectionUpperpp2760GeV = (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionDirGammaNonFitSumErrAr");
 		TGraphAsymmErrors*	graphDRStatpp2760GeV 						    = (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphRGammaNonFitCombStatErr");
 		TGraphAsymmErrors*	graphDRSyspp2760GeV 							  = (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphRGammaNonFitCombSysErr");
-		TGraphAsymmErrors*	graphIncGammaYieldStatpp2760GeV 		= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionIncGammaStatErr");
-		TGraphAsymmErrors*	graphIncGammaYieldSyspp2760GeV 			= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionIncGammaSysErr");
+		TGraphAsymmErrors*	graphIncGammaYieldStatpp2760GeV 		= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvYieldIncGammaStatErr");
+		TGraphAsymmErrors*	graphIncGammaYieldSyspp2760GeV 			= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvYieldIncGammaSysErr");
+		TGraphAsymmErrors*	graphIncGammaXSectionStatpp2760GeV 	= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionIncGammaStatErr");
+		TGraphAsymmErrors*	graphIncGammaXSectionSyspp2760GeV 	= (TGraphAsymmErrors*)directoryCombGammapp2760GeV->Get("graphInvXSectionIncGammaSysErr");
     
     TFile* fCombResultspp8TeV= new TFile(nameFilepp8TeV.Data());
 
 		TDirectory* 		directoryCombGammapp8TeV 					      = (TDirectory*)fCombResultspp8TeV->Get("Gamma8TeV");
-		TGraphAsymmErrors*	graphDirGammaYieldStatpp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionDirGammaNonFitStatErr");
-		TGraphAsymmErrors*	graphDirGammaYieldSyspp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionDirGammaNonFitSysErr");;
-		TGraphAsymmErrors*	graphDirGammaYieldUpperpp8TeV 			= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionDirGammaNonFitSumErrAr");
+		TGraphAsymmErrors*	graphDirGammaYieldStatpp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvYieldDirGammaNonFitStatErr");
+		TGraphAsymmErrors*	graphDirGammaYieldSyspp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvYieldDirGammaNonFitSysErr");;
+		TGraphAsymmErrors*	graphDirGammaYieldUpperpp8TeV 			= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvYieldDirGammaNonFitSumErrAr");
+		TGraphAsymmErrors*	graphDirGammaXSectionStatpp8TeV 		= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionDirGammaNonFitStatErr");
+		TGraphAsymmErrors*	graphDirGammaXSectionSyspp8TeV 			= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionDirGammaNonFitSysErr");;
+		TGraphAsymmErrors*	graphDirGammaXSectionUpperpp8TeV 		= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionDirGammaNonFitSumErrAr");
 		TGraphAsymmErrors*	graphDRStatpp8TeV 						      = (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphRGammaCombNonFitStatErr");
 		TGraphAsymmErrors*	graphDRSyspp8TeV 							      = (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphRGammaCombNonFitSysErr");
-		TGraphAsymmErrors*	graphIncGammaYieldStatpp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionIncGammaStatErr");
-		TGraphAsymmErrors*	graphIncGammaYieldSyspp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionIncGammaSysErr");
+		TGraphAsymmErrors*	graphIncGammaYieldStatpp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvYieldIncGammaStatErr");
+		TGraphAsymmErrors*	graphIncGammaYieldSyspp8TeV 				= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvYieldIncGammaSysErr");
+		TGraphAsymmErrors*	graphIncGammaXSectionStatpp8TeV 		= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionIncGammaStatErr");
+		TGraphAsymmErrors*	graphIncGammaXSectionSyspp8TeV 			= (TGraphAsymmErrors*)directoryCombGammapp8TeV->Get("graphInvXSectionIncGammaSysErr");
 		
 		
 	TString headerPaper = "*author: ACHARYA \n*reference: CERN-PH-EP-XXXX-XXX : 2018\n*status: Encoded 13 Mar 2018 by N. Schmidt\n*experiment: CERN-LHC-ALICE\n*detector: ALICE\n\n*title: Direct photon production at low transverse momentum in proton-proton collisions at $\\sqrt{s}=2.76$ and 8 TeV\n*comment: CERN-LHC. Measurements of inclusive and direct photon production at mid-rapidity in pp collisions at $\\sqrt{s}=2.76$ and 8 TeV are presented by the ALICE experiment at the LHC. The results are reported in transverse momentum ranges of $0.4<p_{\\rm T}<10$ GeV/c and $0.3<p_{\\rm T}<16$ GeV/c, respectively. Photons are detected with the electromagnetic calorimeter (EMCal) and via reconstruction of $e^+e^-$ pairs from conversions in the ALICE detector material using the central tracking system. For the final measurement of the inclusive photon spectra the results are combined in the overlapping $p_{\\rm T}$ interval of both methods. Direct photon spectra, or their upper limits at 90% C.L. are extracted using the direct photon excess ratio $R_\\gamma$, which quantifies the ratio of inclusive photons over decay photons generated with a decay-photon simulation. An additional hybrid method, combining photons reconstructed from conversions with those identified in the EMCal, is used for the combination of the direct photon excess ratio $R_\\gamma$, as well as the extraction of direct photon spectra or their upper limits. While no significant signal of direct photons is seen over the full $p_{\\rm T}$ range, $R_\\gamma$ for $p_{\\rm T}>7$ GeV/c is at least one $\\sigma$ above unity and consistent with expectations from next-to-leading order pQCD calculations.\n";
@@ -111,16 +121,31 @@ void ProduceHEPDataFileForPPDirGammaPaper2018(
 		ProduceHEPDataFileWithUpperLimits(graphDRStatpp8TeV, graphDRSyspp8TeV, NULL, newBinsCombpp8TeV, nPointsTotpp8TeV, headerRAApp8TeV);
     
 		TString headerIncpp2760GeV = "*location: Fig. 5\n*dscomment: Invariant differential cross section of inclusive GAMMA produced in inelastic pp collisions at center-of-mass energy 2.76 TeV, the uncertainty of $\\sigma_{MB}$ of 2.5% is not included in the systematic error.\n*reackey: P P --> GAMMA X\n*obskey: E*D3SIG/D3P\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 2760.0\n*yheader: E d**3sigma/dp**3 IN (pb GEV**-2 c**3) \n*xheader: PT IN GEV/c\n*data: x : y";
-		ProduceHEPDataFileWithUpperLimits(graphIncGammaYieldStatpp2760GeV, graphIncGammaYieldSyspp2760GeV, NULL, newBinsCombpp2760GeV, nPointsTotpp2760GeV, headerIncpp2760GeV);
+		ProduceHEPDataFileWithUpperLimits(graphIncGammaXSectionStatpp2760GeV, graphIncGammaXSectionSyspp2760GeV, NULL, newBinsCombpp2760GeV, nPointsTotpp2760GeV, headerIncpp2760GeV);
     
 		cout << endl;
 		TString headerIncpp8TeV = "*location: Fig. 5\n*dscomment: Invariant differential cross section of inclusive GAMMA produced in inelastic pp collisions at center-of-mass energy 8 TeV, the uncertainty of $\\sigma_{MB}$ of 2.6% is not included in the systematic error.\n*reackey: P P --> GAMMA X\n*obskey: E*D3SIG/D3P\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 8000.0\n*yheader: E d**3sigma/dp**3 IN (pb GEV**-2 c**3) \n*xheader: PT IN GEV/c\n*data: x : y";
-		ProduceHEPDataFileWithUpperLimits(graphIncGammaYieldStatpp8TeV, graphIncGammaYieldSyspp8TeV, NULL, newBinsCombpp8TeV, nPointsTotpp8TeV, headerIncpp8TeV);
+		ProduceHEPDataFileWithUpperLimits(graphIncGammaXSectionStatpp8TeV, graphIncGammaXSectionSyspp8TeV, NULL, newBinsCombpp8TeV, nPointsTotpp8TeV, headerIncpp8TeV);
 	
     TString headerpp2760GeV = "*location: Fig. 5\n*dscomment: Invariant differential cross section of direct GAMMA produced in inelastic pp collisions at center-of-mass energy 2.76 TeV, the uncertainty of $\\sigma_{MB}$ of 2.5% is not included in the systematic error.\n*reackey: P P --> GAMMA X\n*obskey: E*D3SIG/D3P\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 2760.0\n*yheader: E d**3sigma/dp**3 IN (pb GEV**-2 c**3) \n*xheader: PT IN GEV/c\n*data: x : y";
-		ProduceHEPDataFileWithUpperLimits(graphDirGammaYieldStatpp2760GeV, graphDirGammaYieldSyspp2760GeV, graphDirGammaYieldUpperpp2760GeV, newBinsCombpp2760GeV, nPointsTotpp2760GeV, headerpp2760GeV,"(90% CL)");
+		ProduceHEPDataFileWithUpperLimits(graphDirGammaXSectionStatpp2760GeV, graphDirGammaXSectionSyspp2760GeV, graphDirGammaXSectionUpperpp2760GeV, newBinsCombpp2760GeV, nPointsTotpp2760GeV, headerpp2760GeV,"(90% CL)");
     
 		cout << endl;
 		TString headerpp8TeV = "*location: Fig. 5\n*dscomment: Invariant differential cross section of direct GAMMA produced in inelastic pp collisions at center-of-mass energy 8 TeV, the uncertainty of $\\sigma_{MB}$ of 2.6% is not included in the systematic error.\n*reackey: P P --> GAMMA X\n*obskey: E*D3SIG/D3P\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 8000.0\n*yheader: E d**3sigma/dp**3 IN (pb GEV**-2 c**3) \n*xheader: PT IN GEV/c\n*data: x : y";
-		ProduceHEPDataFileWithUpperLimits(graphDirGammaYieldStatpp8TeV, graphDirGammaYieldSyspp8TeV, graphDirGammaYieldUpperpp8TeV, newBinsCombpp8TeV, nPointsTotpp8TeV, headerpp8TeV,"(90% CL)");
+		ProduceHEPDataFileWithUpperLimits(graphDirGammaXSectionStatpp8TeV, graphDirGammaXSectionSyspp8TeV, graphDirGammaXSectionUpperpp8TeV, newBinsCombpp8TeV, nPointsTotpp8TeV, headerpp8TeV,"(90% CL)");
+    
+    // additional data for invariant yield
+		TString headerYieldIncpp2760GeV = "*location: not in paper\n*dscomment: Invariant differential yield of inclusive GAMMA produced in inelastic pp collisions at center-of-mass energy 2.76 TeV.\n*reackey: P P --> GAMMA X\n*obskey: DN/DPT**2/DYRAP\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 2760.0\n*yheader: 1/(2*PI*Nevt)*(d**2N)/(PT*dPT*dY) IN (GEV/c)**-2\n*xheader: PT IN GEV/c\n*data: x : y";
+		ProduceHEPDataFileWithUpperLimits(graphIncGammaYieldStatpp2760GeV, graphIncGammaYieldSyspp2760GeV, NULL, newBinsCombpp2760GeV, nPointsTotpp2760GeV, headerYieldIncpp2760GeV);
+    
+		cout << endl;
+		TString headerYieldIncpp8TeV = "*location: not in paper\n*dscomment: Invariant differential yield of inclusive GAMMA produced in inelastic pp collisions at center-of-mass energy 8 TeV.\n*reackey: P P --> GAMMA X\n*obskey: DN/DPT**2/DYRAP\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 8000.0\n*yheader: 1/(2*PI*Nevt)*(d**2N)/(PT*dPT*dY) IN (GEV/c)**-2\n*xheader: PT IN GEV/c\n*data: x : y";
+		ProduceHEPDataFileWithUpperLimits(graphIncGammaYieldStatpp8TeV, graphIncGammaYieldSyspp8TeV, NULL, newBinsCombpp8TeV, nPointsTotpp8TeV, headerYieldIncpp8TeV);
+	
+    TString headerYieldpp2760GeV = "*location: not in paper\n*dscomment: Invariant differential yield of direct GAMMA produced in inelastic pp collisions at center-of-mass energy 2.76 TeV.\n*reackey: P P --> GAMMA X\n*obskey: DN/DPT**2/DYRAP\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 2760.0\n*yheader: 1/(2*PI*Nevt)*(d**2N)/(PT*dPT*dY) IN (GEV/c)**-2\n*xheader: PT IN GEV/c\n*data: x : y";
+		ProduceHEPDataFileWithUpperLimits(graphDirGammaYieldStatpp2760GeV, graphDirGammaYieldSyspp2760GeV, graphDirGammaYieldUpperpp2760GeV, newBinsCombpp2760GeV, nPointsTotpp2760GeV, headerYieldpp2760GeV,"(90% CL)");
+    
+		cout << endl;
+		TString headerYieldpp8TeV = "*location: not in paper\n*dscomment: Invariant differential yield of direct GAMMA produced in inelastic pp collisions at center-of-mass energy 8 TeV.\n*reackey: P P --> GAMMA X\n*obskey: DN/DPT**2/DYRAP\n*qual: RE: P P --> GAMMA X\n*qual: YRAP : 0.0\n*qual: SQRT(S) IN GEV : 8000.0\n*yheader: 1/(2*PI*Nevt)*(d**2N)/(PT*dPT*dY) IN (GEV/c)**-2\n*xheader: PT IN GEV/c\n*data: x : y";
+		ProduceHEPDataFileWithUpperLimits(graphDirGammaYieldStatpp8TeV, graphDirGammaYieldSyspp8TeV, graphDirGammaYieldUpperpp8TeV, newBinsCombpp8TeV, nPointsTotpp8TeV, headerYieldpp8TeV,"(90% CL)");
 }
