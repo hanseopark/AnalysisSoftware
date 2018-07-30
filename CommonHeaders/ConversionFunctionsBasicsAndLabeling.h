ebeaec355ce91e44de020832aefa79db27db083c
Remco de Boer <remco.deboer@cern.ch>
2018-07-30 09:47:47 +0000

    Update ConversionFunctionsBasicsAndLabeling.h

--- CommonHeaders/ConversionFunctionsBasicsAndLabeling.h
+++ CommonHeaders/ConversionFunctionsBasicsAndLabeling.h
@@ -567,6 +567,7 @@
     //**** Decodes from the mode the respective reco process and return correct label ****
     //************************************************************************************
     TString ReturnTextReconstructionProcess(Int_t mode){
+        mode -= 100;
         switch (mode){
             case 0:
                 return "PCM";
@@ -617,16 +618,6 @@
                 return "#pi^{0} rec w/ PHOS, DALITZ";
             case 50:
                 return "#pi^{0} rec w/ DCAL, DALITZ";
-            case 100:
-                return "PCM heavy meson";
-            case 101:
-                return "PCM-#gamma^{*}#gamma heavy meson";
-            case 102:
-                return "PCM-EMC heavy meson";
-            case 103:
-                return "PCM-PHOS heavy meson";
-            case 104:
-                return "EMC heavy meson";
             default:
                 return "not known";
         }
