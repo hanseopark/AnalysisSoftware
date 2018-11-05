99a6a95ec0ba396400334b9ee50dc26841e2f3bd
amarin <amarin@cern.ch>
2018-11-05 12:35:24 +0100

    root6 fix

--- CommonHeaders/ConversionFunctions.h
+++ CommonHeaders/ConversionFunctions.h
@@ -205,13 +205,22 @@
         Double_t    xMin, xMax;
         TString     formula         = func->GetExpFormula();
         func->GetRange(xMin, xMax);
-
+#ifdef __CLING__
+        for (Int_t i=0; i<func->GetNpar(); i++) {
+            formula.ReplaceAll(Form("[p%d]", i), Form("[placeholder%d]",i+1));
+        }
+        for (Int_t i=1; i<func->GetNpar()+1; i++) {
+            formula.ReplaceAll(Form("[placeholder%d]", i), Form("[p%d]",i));
+        }
+#else
         for (Int_t i=0; i<func->GetNpar(); i++) {
             formula.ReplaceAll(Form("[%d]", i), Form("[placeholder%d]",i+1));
         }
         for (Int_t i=1; i<func->GetNpar()+1; i++) {
             formula.ReplaceAll(Form("[placeholder%d]", i), Form("[%d]",i));
         }
+
+  #endif
 
         TF1* result                 = new TF1(name.Data(), Form("[0] * (%s)", formula.Data()), xMin, xMax);
         for (Int_t i=0; i<func->GetNpar()+1; i++) {
