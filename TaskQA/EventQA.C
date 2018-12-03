77d175df961ca95da2483b99ff48588c2e1e624f
Jens Lühder <jluhder@cern.ch>
2018-12-03 09:48:54 +0100

    Delete Line because of Merge Conflict

--- TaskQA/EventQA.C
+++ TaskQA/EventQA.C
@@ -104,7 +104,6 @@ void EventQA(
 
     vector <TString> cutsTemp;
     map<TString,Int_t> mapCuts;
-    TString TStrNameKey;
 
     for(Int_t i=0; i<nSets; i++){
         TFile *fFile = new TFile(pathDataSets[i].Data(),"READ");
